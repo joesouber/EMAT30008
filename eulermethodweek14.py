ece761faefafa16d61536c2ef72524f1f9e4a12f
joesouber <78925833+joesouber@users.noreply.github.com>
2023-02-06 14:53:48 +0000

    -

--- eulermethodweek14.py
+++ eulermethodweek14.py
@@ -20,7 +20,7 @@ def rk4_step(x, t, dt, f):
     k1 = f(x, t)
     k2 = f(x + dt/2 * k1, t + dt/2)
     k3 = f(x + dt/2 * k2, t + dt/2)
-    k4 = f(x + dt * k3, t + dt)
+    k4 = f(x + dt * k3, t + dt) 
     x_i_1 = x + dt/6 * (k1 + 2 * k2 + 2 * k3 + k4)
     return x_i_1
 
