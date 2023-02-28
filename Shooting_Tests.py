6e149b7e34f489027fe2b9419ab4c46814b53b6f
joesouber <78925833+joesouber@users.noreply.github.com>
2023-02-28 16:25:53 +0000

    testing hopf normal form, NEEDS WORK

--- Shooting_Tests.py
+++ Shooting_Tests.py
@@ -1,74 +1,95 @@
-#Shooting Tests.
 import numpy as np
-from week15 import shooting, pc_predator_prey
+import week15
+from scipy.integrate import solve_ivp
+from scipy.optimize import fsolve
 
-def shooting_tester(shooting_func, ODE, true_ODE, u0, pc, system, *args):
+# ode to solve using shooting
+def hopf(t, u):
+
+    u1, u2 = u
+    beta = 1
+    sigma = -1
+
+    du1dt = beta*u1 - u2 + sigma*u1*(u1**2 + u2**2)
+    du2dt = u1 + beta*u2 + sigma*u2*(u1**2 + u2**2)
+
+    return np.array((du1dt, du2dt))
+
+
+
+# exact solution to differential equation
+def true_hopf(t):
+    beta = 1
+    theta = 0.0
+
+    u1 = np.sqrt(beta) * np.cos(t + theta)
+    u2 = np.sqrt(beta) * np.sin(t + theta)
+
+    return np.array([u1, u2])
+
+
+
+# phase condition function for the hopf
+def pc(u0, *args):
+    return hopf(0, u0, *args)[0]
+
+
+def test_limit_cycle(test_f, true_f, U0, pc, tolerance, endpoints,*args):
     """
-    Finds the accuracy of a shooting solution compared to the true solution
-        Parameters:
-            shooting (function):   this is the shooting function, but to avoid circular imports, the function could not be imported into this code
-            ODE (function):             the ODE to test the solutions on
-            true_ODE (function):        the true solution function of the ODE
-            u0:                         initial conditions x0 and t
-            pc (function):              the phase condition
-            system (bool):              True if the ODE is a system of equations, False otherwise
-            *args:                      any additional arguments that the ODE function defined above expects
-        Returns:
-            The accuracy of the true solution, as a power of 10.
-    """
-    empirical_sol = shooting_func(ODE, u0, pc, system, False, *args)
-    true_sol = true_ODE(u0[-1], *args)
+    Function testing limit cycle solutions against the true value at a given tolerance, 
+    can be specified to output the closeness of the initial conditions and endpoints of the 
+    limit cycle to test if a full loop is formed"""
 
-    tolerance_values = [10**x for x in list(range(-10, 1, 1))]
+    # find limit cycle conditions of given function to test
+    soll = week15.shooting(test_f, U0, pc,fsolve, *args)
+     
+    # unpack solution
+    u0 = soll[:-1]
+    T = soll[-1]
 
-    proximity = False
-    i = 0
+    # Use solve_ivp to get limit cycle values
+    sol = solve_ivp(test_f,(0,T),u0)
+    t_sol = sol.t
+    u_sol = sol.y
 
-    while not proximity:
-        if system:
-            proximity = np.allclose(empirical_sol[0][-1, :], true_sol, rtol=tolerance_values[i])
+    # transpose array to fit true_f dimensions
+    u_sol = u_sol.T
 
-        else:
-            proximity = np.isclose(empirical_sol[0][-1], true_sol, rtol=tolerance_values[i])
+    # get array shape to create according zero array
+    rows, columns = u_sol.shape
+    exact_sol = np.zeros([rows, columns])
 
-        i += 1
-
-        if i == 11:
-            raise ValueError(f"The empirical solution was never close to the true solution, to a tolerance of 1. Please try again with a different ODE or check your functions.")
-
-    return tolerance_values[i - 1]
+    # get true solutions to the ode for the same times.
+    for i in range(0,len(t_sol)):
+        exact_sol[i] = true_f(t_sol[i])
 
 
+    if endpoints == False:
+        # compare true solutions to solutions obtained using the limit cycle.
+        return np.allclose(u_sol, exact_sol, tolerance)
+    elif endpoints == True:
+        # compare initial conditions with limit cycle endpoints
+        return np.allclose(exact_sol[0,:], exact_sol[-1,:], tolerance)
+    
 
 
-#%% Hopf Bifurcation tests for shooting
+if __name__ == '__main__':
 
+    print('\n')
+    
+    tolerance = np.logspace(-1,-21,21)  # tolerance range to test for
+    
+    # initial condition estimates
+    U0_2d = (-1,0,6) 
+    
 
-def test():
+    # test found limit cycle matches true values at same time points
+    # test 2D ode
+    for i in range(0,len(tolerance)):
+        passed = test_limit_cycle(hopf,true_hopf,U0_2d,pc,tolerance[i],False)
+        if passed == False:
+            print(f"Limit cycle is found to be accurate at a tolerance level of {tolerance[i-1]} for a 2 dimensional ODE\n")
+            break
+        elif i == len(tolerance):
+            print(f"Limit cycle is found to be accurate at a tolerance level of {tolerance[-1]} or greater, for a 2 dimensional ODE\n")
 
-    def Hopf_bif(U, t, args):
-        beta = args[0]
-        sigma = args[1]
-
-        u1, u2 = U
-        du1dt = beta * u1 - u2 + sigma * u1 * (u1 ** 2 + u2 ** 2)
-        du2dt = u1 + beta * u2 + sigma * u2 * (u1 ** 2 + u2 ** 2)
-        dudt = np.array([du1dt, du2dt])
-
-        return dudt
-
-    def true_Hopf_bif(t, args):
-        beta = args[0]
-        phase = args[1]
-
-        u1 = np.sqrt(beta) * np.cos(t + phase)
-        u2 = np.sqrt(beta) * np.sin(t + phase)
-
-        return np.array([u1, u2])
-
-    shooting_tester(shooting, Hopf_bif, true_Hopf_bif, [1.2, 1.2, 8], pc_predator_prey, True, [1, -1])
-
-
-if __name__ == "__main__":
-    # numerical_shooting_tests()
-    test()
