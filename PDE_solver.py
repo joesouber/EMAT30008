55c3233122c703969dbde21b11eb825bc071b28c
joesouber <78925833+joesouber@users.noreply.github.com>
2023-03-27 15:07:46 +0100

    Adding a_dd

--- PDE_solver.py
+++ PDE_solver.py
@@ -17,8 +17,42 @@ def solve_pde(u_I, L, T, D, bound_left, bound_right ,boundary, method, mt=1000, 
     deltat = t[1] - t[0]            # gridspacing in t
     f_mesh = D*deltat/(deltax**2)    # mesh fourier number
 
+    def matrix_dimensions(boundary):
+
+        if boundary == 'dirichlet':
+            dimension = mx - 1
+            u_j = np.zeros(x.size)
+            for i in range(0,len(u_j)):
+                u_j[i] = u_I(x[i])
+            
+            return dimension, u_j
 
 
+        if boundary == 'neumann':
+            dimension = mx + 1
+            u_j = np.zeros(x.size)
+            for i in range(0,len(u_j)):
+                u_j[i] = u_I(x[i])
+            return dimension, u_j
 
+    def A_dd_matrix(method, dimension, boundary):
+        """ 
+        In-bedded function which returns the appropriate tri-diagonal matrix in function 
+        of the boudary condition and methid specified.
+        """
+        # Set tridiagonal matrix 
+        # forward euler
+        if method == 'forward':
+            diag = [[f_mesh] * (dimension-1), [1 - 2*f_mesh] * dimension , [f_mesh] * (dimension-1)]
+            A_dd = diags(diag, offsets = [-1,0,1], format = 'csc')
+            if boundary == 'dirichlet':
+                return A_dd, None
+
+            elif boundary == 'neumann':
+                A_dd = A_dd.toarray()
+                A_dd[0,1] *= 2
+                A_dd[-1,-2] *= 2   
+                return csr_matrix(A_dd), None
+            
 
 
