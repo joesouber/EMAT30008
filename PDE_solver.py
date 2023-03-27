29bfd4a128e95927c1e20ab0dced673a73c8ac3d
joesouber <78925833+joesouber@users.noreply.github.com>
2023-03-27 15:32:55 +0100

    PDE solver for explicitE, with dirichlet + neumann

--- PDE_solver.py
+++ PDE_solver.py
@@ -35,15 +35,15 @@ def solve_pde(u_I, L, T, D, bound_left, bound_right ,boundary, method, mt=1000, 
                 u_j[i] = u_I(x[i])
             return dimension, u_j
 
-    def A_dd_matrix(method, dimension, boundary):
+    def A_dd_matrix(method, matrix_dim, boundary):
         """ 
         In-bedded function which returns the appropriate tri-diagonal matrix in function 
         of the boudary condition and methid specified.
         """
         # Set tridiagonal matrix 
-        # forward euler
-        if method == 'forward':
-            diag = [[f_mesh] * (dimension-1), [1 - 2*f_mesh] * dimension , [f_mesh] * (dimension-1)]
+        # explicit euler
+        if method == 'explicit':
+            diag = [[f_mesh] * (matrix_dim-1), [1 - 2*f_mesh] * matrix_dim , [f_mesh] * (matrix_dim-1)]
             A_dd = diags(diag, offsets = [-1,0,1], format = 'csc')
             if boundary == 'dirichlet':
                 return A_dd, None
@@ -53,6 +53,57 @@ def solve_pde(u_I, L, T, D, bound_left, bound_right ,boundary, method, mt=1000, 
                 A_dd[0,1] *= 2
                 A_dd[-1,-2] *= 2   
                 return csr_matrix(A_dd), None
+        
+        
+
+                   
+
+    dimension, u_sol = matrix_dimensions(boundary)
+    u_1 = np.zeros(mx+1)
+    
+   
+    def b_dd(boundary):
+
+        if boundary == 'dirichlet':
+            return np.zeros(mx-1)
+        if boundary == 'neumann':
+            return np.zeros(mx+1)
+        
+    # setup additive vector and appropriate matrix dimensions
+   
+    dimension, u_sol = matrix_dimensions(boundary)
+    u_1 = np.zeros(mx+1)
+    B_dd = b_dd(boundary)
+    diag1 = A_dd_matrix(method, dimension, boundary)
+    
+    # Solve PDE for each time value
+    for i in range(0,mt):
+        # forwad euler matrix calc
+        if boundary == 'dirichlet':
+            B_dd[0], B_dd[-1] = bound_left(t[i]), bound_right(t[i])
+            if method == 'explicit':
+                u_1[1:-1] = diag1.dot(u_sol[1:-1]) + B_dd * f_mesh
+
+
+            # add boundary conditions
+            u_1[0] = B_dd[0]
+            u_1[-1] = B_dd[-1]
+            #initialise u_j for the next time step
+            u_sol[:] = u_1[:]
+
+
+        if boundary == 'neumann':
+            B_dd[0], B_dd[-1] = -bound_left(t[i]), bound_right(t[i])
+            if method == 'explicit':
+                u_1 = diag1.dot(u_sol) + 2 * f_mesh * deltax * B_dd
+            
+
+
+
+
+
+    return x, u_sol
+            
             
 
 
