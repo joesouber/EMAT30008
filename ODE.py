374adba97c5bb8d31d77c4c40c6235bb3ab82d5e
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-09 15:13:03 +0100

    adding func back

--- ODE.py
+++ ODE.py
@@ -77,3 +77,46 @@ def solve_ode(f, x0, t_eval, max_step, method, system, *pars):
         X = X.transpose()
     
     return X
+
+def calculate_error(f, true_sol, x0, t0, t1, dt_list, *args):
+        """
+        Calculate the error between the approximate solution obtained using Euler, RK4 or Heun method and the true solution
+        for different timesteps.
+
+        Parameters:
+        f (function): Function that returns the derivative of the function to be solved.
+        true_sol (function): Function that returns the true solution of the ODE.
+        x0 (float): Initial value of the function.
+        t0 (float): Initial time.
+        t1 (float): Final time.
+        dt_list (list): List of timestep sizes to compare.
+        args (tuple): Tuple of additional arguments for f.
+
+        Returns:
+        dict: Dictionary containing the errors for each method.
+        """
+        # Define the approximate methods to compare
+        methods = ['euler', 'RK4', 'heun']
+
+        # Define the dictionary to store the errors
+        errors = {}
+        for method in methods:
+            errors[method] = []
+
+        # Loop over the timestep sizes
+        for dt in dt_list:
+            # Compute the approximate solutions
+            X_euler = solve_ode(f, x0, np.arange(t0, t1 + dt, dt), dt, 'euler', False, *args)[-1]
+            X_RK4 = solve_ode(f, x0, np.arange(t0, t1 + dt, dt), dt, 'RK4', False, *args)[-1]
+            X_heun = solve_ode(f, x0, np.arange(t0, t1 + dt, dt), dt, 'heun', False, *args)[-1]
+
+            # Compute the true solution
+            T = np.arange(t0, t1 + dt, dt)
+            X_true = true_sol(T)
+
+            # Compute the errors
+            errors['euler'].append(np.abs(X_euler - X_true)[-1])
+            errors['RK4'].append(np.abs(X_RK4 - X_true)[-1])
+            errors['heun'].append(np.abs(X_heun - X_true)[-1])
+
+        return errors
