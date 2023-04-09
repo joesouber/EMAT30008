6f428987ffd9c7c7e7b99f19c1781a11a984001e
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-09 15:39:13 +0100

    un-embed func.

--- plots.py
+++ plots.py
@@ -1,30 +1,9 @@
+#%%
 from matplotlib import pyplot as plt
 import numpy as np
 from ODE import solve_ode
 
-def plot_error(f, f_true_solution, x0, t0, t1, dt_list, *args):
-    """
-    Plots the error for the Euler, RK4, and Heun methods for different timesteps.
-
-    Parameters:
-    -----------
-    f : callable
-        The function defining the ODE system, must take inputs (x, t, *args).
-    true_sol : callable
-        A function that returns the true solution to the ODE system, must take inputs (t, x0, *args).
-    x0 : float or list
-        The initial condition(s) for the ODE system.
-    t0 : float
-        The starting time for the solution.
-    t1 : float
-        The end time for the solution.
-    dt_list : list
-        A list of timesteps to use for the numerical solutions.
-    *args : tuple, optional
-        Extra arguments to be passed to `f` and `true_sol`.
-
-    """
-    def calculate_error(f, f_true_solution, x0, t0, t1, dt_list, *args):
+def calculate_error(f, f_true_solution, x0, t0, t1, dt_list, *args):
         """
         Calculate the error between the approximate solution obtained using Euler, RK4 or Heun method and the true solution
         for different timesteps.
@@ -67,6 +46,30 @@ def plot_error(f, f_true_solution, x0, t0, t1, dt_list, *args):
 
         return errors
 
+
+def plot_error(f, f_true_solution, x0, t0, t1, dt_list, *args):
+    """
+    Plots the error for the Euler, RK4, and Heun methods for different timesteps.
+
+    Parameters:
+    -----------
+    f : callable
+        The function defining the ODE system, must take inputs (x, t, *args).
+    true_sol : callable
+        A function that returns the true solution to the ODE system, must take inputs (t, x0, *args).
+    x0 : float or list
+        The initial condition(s) for the ODE system.
+    t0 : float
+        The starting time for the solution.
+    t1 : float
+        The end time for the solution.
+    dt_list : list
+        A list of timesteps to use for the numerical solutions.
+    *args : tuple, optional
+        Extra arguments to be passed to `f` and `true_sol`.
+
+    """
+
     # Calculate the errors for the three methods using the given parameters
     errors = calculate_error(f, f_true_solution, x0, t0, t1, dt_list, *args)
 
@@ -80,3 +83,6 @@ def plot_error(f, f_true_solution, x0, t0, t1, dt_list, *args):
     plt.title('Errors for Numerical Methods')
     plt.legend()
     plt.show()
+
+
+
