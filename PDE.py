b97e4a7ba9f2607ab0bf38733ecf7b99102337d5
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-17 13:25:31 +0100

    tridiagonal matrix

--- PDE.py
+++ PDE.py
@@ -4,33 +4,29 @@ from scipy.sparse import diags, identity
 from scipy.sparse.linalg import spsolve
 from scipy.optimize import fsolve
 
-def construct_L(u, j, L, t, x, D, BC_type):
-    delta_x = x[1] - x[0]
-    delta_t = t[1] - t[0]
+def tridiag_mat(pde_sol, BC_type, t, x, D,L,T=0.5,mt=1000,mx=100):
+#possibly done
+    delta_t = T / mt
+    delta_x = L / mx
 
-    subdiag = delta_t/(delta_x**2) * D(x + delta_x/2)[1:]
-    diag = delta_t/(delta_x**2) * (D(x+delta_x/2) + D(x-delta_x/2))
-    superdiag = delta_t/(delta_x**2) * D(x-delta_x/2)[:-1]
+    n = len(pde_sol)
 
-    off_diagonal_boundary_coefficient = np.ones(u.shape[0]-1)
-    diag_boundary_coefficient = -np.ones(u.shape[0])
+    subdiag = delta_t/(delta_x**2) * D*(x + delta_x/2)[1:]
+    centre_diag = delta_t/(delta_x**2) * (D*(x+delta_x/2) + D*(x-delta_x/2))
+    superdiag = delta_t/(delta_x**2) * D*(x-delta_x/2)[:-1]
+    
+    if BC_type == 'dirichlet':
+        
+        np.ones(n-1)[-1] = 0
+        np.array([-1]*n)[0] = 0
+        np.array([-1]*n)[-1] = 0
 
-    boundary_coefficients = {
-        'dirichlet': {
-            'off_diagonal': 0,
-            'diagonal': 0,
-        },
-        'neumann': {
-            'off_diagonal': 2,
-            'diagonal': -1,
-        }
-    }
-
-    boundary_coef = boundary_coefficients[BC_type]
-    off_diagonal_boundary_coefficient[-1] = boundary_coef['off_diagonal']
-    diag_boundary_coefficient[0] = boundary_coef['diagonal']
-    diag_boundary_coefficient[-1] = boundary_coef['diagonal']
-
-    L = diags((subdiag*off_diagonal_boundary_coefficient, diag*diag_boundary_coefficient, superdiag*off_diagonal_boundary_coefficient), (-1,0,1), format='csr')
-
-    return L
+    elif BC_type == 'neumann':
+        
+        np.ones(n-1)[-1] = 2
+    
+    diagonals = (subdiag*np.ones(n-1), centre_diag*np.array([-1]*n), superdiag*np.ones(n-1))
+    offset = (-1,0,1)
+    
+    tridiag_mat = diags(diagonals, offset, format='csr')
+    return tridiag_mat
