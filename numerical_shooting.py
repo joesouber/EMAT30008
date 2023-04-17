c210fbd5d45b67bc25aa30266c92d8479c719946
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-17 12:17:25 +0100

    removed func

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -7,36 +7,7 @@ import matplotlib.pyplot as plt
 import numdifftools as nd
 from ODE import solve_ode
 #%%
-def pred_prey_eq(X, pars):
-  
-    x = X[0]
-    y = X[1]
-    a, b, d = pars[0], pars[1], pars[2]
-    dxdt = x * (1 - x) - (a * x * y) / (d + x)
-    dydt = b * y * (1 - (y / x))
-    return np.array([dxdt, dydt])
 
-## simulating pred/prey equations uysing scipy solve_ivp. Should be able to switch this out for one of my functions eventually.
-
-a = 1
-d = 0.1
-bs = [0.1,0.5]
-
-
-def pred_prey_plot(a,d,bs):
-    for b in bs:
-        pars = [a, b, d]
-        X0 = [0.5, 0.5]
-        t_eval = np.linspace(0, 200, 3000)
-        sol = solve_ivp(lambda t, X: pred_prey_eq(X, pars), [0, 200], X0, t_eval=t_eval)
-
-
-        plt.plot(sol.t, sol.y[0], label='Predator population')
-        plt.plot(sol.t, sol.y[1], label='Prey population')
-        plt.legend()
-        plt.xlabel('Time')
-        plt.ylabel('Population')
-        plt.show()
 
 #To isolate a periodic orbit, we need to look for a closed trajectory in the phase space of the predator-prey system. One way to do this is to plot the phase portrait of the system, which shows the direction of motion of the system's trajectories. We can then identify any closed trajectories, which correspond to periodic orbits.
 #%%
