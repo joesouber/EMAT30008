fbd1a9b6fd589784392ab4201f5914bad6931a87
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-20 14:32:57 +0100

    shooting input tests

--- test_solutions.py
+++ test_solutions.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from shooting_plot import shooting_generalised, plot_hopf_shooting
 from scipy.integrate import odeint
+from plots import plot_solutions
 #ode
 def test_solution(tolerance):
     # Define the function to be solved
@@ -34,6 +35,92 @@ def test_solution(tolerance):
         print(f"{method.upper()}: Numerical and exact solutions match closely: {is_close}")
 
 #shooting
+#%%
+def test_plot_solutions():
+    def predator_prey(t, y, args = [1, 0.1, 0.1]):
+        x = y[0]
+        y = y[1]
+
+        a = args[0]
+        d = args[1]
+        b = args[2]
+        
+        dxdt = x * (1 - x) - (a * x * y) / (d + x)
+        dydt = b * y * (1 - (y / x))
+        return np.array([dxdt, dydt])
+    
+    def pc_predator_prey(u0, args = [1, 0.1, 0.1]):
+        return predator_prey(0, u0, args = [1, 0.1, 0.1])[0]
+
+# Define the initial guess for the shooting method
+    u0 = [0.8, 0.3,30]
+    failed_tests = []
+
+    # Test with correct inputs
+    try:
+        plot_solutions(predator_prey, pc_predator_prey, u0)
+    except Exception as e:
+        failed_tests.append("Test with correct inputs passed")
+
+    # Test with incorrect ODE output
+    def incorrect_ode_output(t, y, args=[1, 0.1, 0.1]):
+        return y
+    try:
+        plot_solutions(incorrect_ode_output, pc_predator_prey, u0)
+        failed_tests.append("Test with incorrect ODE output did not raise an exception.")
+    except AssertionError:
+        pass
+    except Exception as e:
+        failed_tests.append("Test with incorrect ODE output raised an  exception")
+
+    # Test with incorrect phase condition output
+    def incorrect_pc_output(u0, args=[1, 0.1, 0.1]):
+        return np.array([1, 2])
+    try:
+        plot_solutions(predator_prey, incorrect_pc_output, u0)
+        failed_tests.append("Test with incorrect phase condition output did not raise an exception.")
+    except AssertionError:
+        pass
+    except Exception as e:
+        failed_tests.append("Test with incorrect phase condition output raised an  exception ")
+
+    # Test with incorrect u0 size
+    u0_incorrect_size = [0.8, 0.3]
+    try:
+        plot_solutions(predator_prey, pc_predator_prey, u0_incorrect_size)
+        failed_tests.append("Test with incorrect u0 size did not raise an exception.")
+    except AssertionError:
+        pass
+    except Exception as e:
+        failed_tests.append("Test with incorrect u0 size raised an  exception ")
+
+    # Test with incorrect step size type
+    max_step_incorrect_type = '0.01'
+    try:
+        plot_solutions(predator_prey, pc_predator_prey, u0, max_step=max_step_incorrect_type)
+        failed_tests.append("Test with incorrect step size type did not raise an exception.")
+    except AssertionError:
+        pass
+    except Exception as e:
+        failed_tests.append("Test with incorrect step size type raised an  exception ")
+
+    if len(failed_tests) == 0:
+        print("All tests passed!")
+    else:
+        print('Some input tests failed:')
+        for test in failed_tests:
+            print(test)
+
+
+test_plot_solutions()
+
+
+
+
+
+#%%
+
+
 def test_shooting(tolerance):
     def normal_hopf(u0, t, beta):
 
