14392fc03a7f44215b6468d3dd2d8634ee6c6629
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-24 17:26:52 +0100

    docstrings and using dictionary!

--- numerical_continuation.py
+++ numerical_continuation.py
@@ -9,25 +9,49 @@ from scipy.optimize import fsolve
 
 
 
-def npc(f, u0, pars, vary_min, discretisation='shooting',solver=fsolve, pc='none'):
-    
-    # Generate an array of parameter values to use in the continuation. Done back-to-front of sorts. 
-    #step set to 50, can reduce this to reduce time, however reduces resolution.
+def npc(f, u0, pars, vary_min, discretisation='shooting', solver=fsolve, pc='none'):
+    """
+    Perform numerical path continuation on a system of ODEs.
+
+    Parameters:
+    -----------
+    f: callable
+        The system of ODEs.
+    u0: array-like
+        The initial conditions for the ODEs.
+    pars: array-like
+        The parameters of the ODE system.
+    vary_min: float
+        The minimum value of the varying parameter.
+    discretisation: str, optional
+        The discretization method for the continuation (default is 'shooting').
+    solver: callable, optional
+        The solver for the continuation (default is fsolve).
+    pc: callable or str, optional
+        The function for parameter continuation (default is 'none').
+
+    Returns:
+    --------
+    parameter_array: numpy array
+        Array of parameter values for the continuation.
+    sol_list: numpy array
+        Array of solution points for the continuation.
+    """
+
+    # Generate an array of parameter values to use in the continuation.
     parameter_array = np.linspace(pars[0], vary_min, 30)
 
     # Initialize an empty list to store the solutions.
     sol_list = []
-    
+
     # Iterate over the parameter values.
     for i in parameter_array:
         # Update the parameter value in `pars`.
         pars.__setitem__(0, i)
-  
-        
+
         # Set the initial conditions for the ODE system.
         initial_pars0 = (pc or '') and ((pc or '') != 'none') and (pc, pars) or pars
 
-        
         # Solve the ODE system using the specified method and solver.
         sol = np.array(solver(discretisation(f), u0, args=initial_pars0))
 
@@ -36,67 +60,77 @@ def npc(f, u0, pars, vary_min, discretisation='shooting',solver=fsolve, pc='none
 
         # Set the initial conditions for the next step to be the current solution.
         u0 = sol
-    
+
     # Return the parameter values and the list of solutions as a tuple.
     return parameter_array, np.array(sol_list)
 
-def pseudo_arclength(f, initial_conds, parameters, param_step, discretisation):
 
+def pseudo_arclength(ode_func, init_conds, param_bounds, step_size, discretisation):
+    """
+    Perform pseudo-arclength continuation on a system of ODEs.
 
-    # Unpack parameters and find the second alpha value, and the stopping condition (alpha_end is reached)
-    start_value, end_value = parameters
-    value_list = [start_value, start_value + param_step]
+    Parameters:
+    -----------
+    ode_func: callable
+        The system of ODEs.
+    init_conds: array-like
+        The initial conditions for the ODEs.
+    param_bounds: tuple
+        The starting and ending values for the parameter.
+    step_size: float
+        The step size for the continuation.
+    discretisation: callable
+        The discretisation function for the continuation.
 
-    # Determine the stopping condition function based on the parameter range
-    if end_value > start_value:
-        def end_init(K):
-            return K > end_value
-    else:
-        def end_init(K):
-            return K < end_value
+    Returns:
+    --------
+    param_list: list
+        List of parameter values for the continuation.
+    solution_points: list
+        List of solution points for the continuation.
+    """
 
+    param_start, param_end = param_bounds
+    param_list = [param_start, param_start + step_size]
 
-    def func_u0(U, f):
-        return discretisation(U, f, start_value)
+    # Create a dictionary to store lambda functions and nested functions
+    function_map = {
+        'should_stop': lambda K: (param_end > param_start and K > param_end) or (param_end < param_start and K < param_end),
+        'first_solution': lambda U: discretisation(U, ode_func, param_start),
+        'second_solution': lambda U: discretisation(U, ode_func, param_list[1]),
+        'arclength_constraint': lambda next_step, prediction, delta: np.dot(next_step - prediction, delta),
+        'combined_equations': lambda next_step: np.append(discretisation(next_step[1:], ode_func, next_step[0]), function_map['arclength_constraint'](next_step, prediction, delta)),
+    }
 
-    u0 = fsolve(func_u0, initial_conds, args=(f,))
-    
-    def func_u1(U,f):
-        return discretisation(U, f, value_list[1])
-    
-    u1 = fsolve(func_u1,u0,args=(f,))
+    # Find the first two solution points using fsolve
+    first_point = fsolve(function_map['first_solution'], init_conds)
+    second_point = fsolve(function_map['second_solution'], first_point)
 
+    solution_points = [first_point, second_point]
 
-    
+    # Loop until the stopping condition is met
+    while True:
+        # Calculate the previous and current points and their difference (delta)
+        previous = np.concatenate(([param_list[-2]], solution_points[-2]))
+        current = np.concatenate(([param_list[-1]], solution_points[-1]))
+        delta = current - previous
 
-    # Solve until the final alpha value is reached using pseudo-arclength continuation
-    U_list = [u0, u1]
-    for i in range(2, 10000):
-    # Check if the stopping condition has been reached
-        if end_init(value_list[-1]):
+        # Predict the next solution point and solve the combined_equations
+        prediction = current + delta
+        next_solution = fsolve(function_map['combined_equations'], prediction)
+
+        # Append the new solution point and parameter value
+        solution_points.append(next_solution[1:])
+        param_list.append(next_solution[0])
+
+        # Check if the stopping condition is met, and break the loop if it is
+        if function_map['should_stop'](param_list[-1]):
             break
 
-    # Generate the secant
-        nu0 = np.concatenate(([value_list[i-2]], U_list[i-2]))
-        nu1 = np.concatenate(([value_list[i-1]], U_list[i-1]))
-        delta = nu1 - nu0
+    return param_list, solution_points
 
-        # Predict solution
-        nu2_ = nu1 + delta
-        
-        def pseudo_arc_equation(nu2):
-            return np.dot(nu2 - nu2_, delta)
 
-# Define the function to pass to fsolve
-        def func(nu2, f):
-            return np.append(discretisation(nu2[1:], f, nu2[0]), pseudo_arc_equation(nu2))
 
-# Solve the function
-        sol = fsolve(func, nu2_, args=(f,))
-        U_list.append(sol[1:])
-        value_list.append(sol[0])
-
-    return value_list, U_list
 
 
 #
