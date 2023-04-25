298c930c055292e3870da0e39e83d9b120d9de21
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-25 14:54:42 +0100

    bugfix

--- PDE.py
+++ PDE.py
@@ -346,7 +346,7 @@ def finite_difference(L, T, mx, mt, Boundary_type, Boundary_Cond, Initial_C, dis
     pde_sol = np.zeros(shape=(len(x), len(t)))
     
     # Check if nonlinear PDE is being solved with the correct method
-    if linearity == 'nonlinear' and discretisation != 'implicit' or 'cn':
+    if linearity == 'nonlinear' and discretisation != 'implicit':
         raise ValueError("Nonlinear equations can only be solved using the Implicit Euler and Crank-Nicholson methods.")
     
     # Determine the finite difference method to use
