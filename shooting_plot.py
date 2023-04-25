630c0e5bf1fe645fefe7ee3635ade8bd1ae6bfbd
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-25 18:20:59 +0100

    tidying

--- shooting_plot.py
+++ shooting_plot.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.integrate import odeint
-
+import numdifftools as nd
+from ODE import solve_ode
 
 
 def plot_solutions(ode,pc,u0,args=(), max_step=0.01):
@@ -23,17 +24,22 @@ def plot_solutions(ode,pc,u0,args=(), max_step=0.01):
             assert isinstance(x0, np.ndarray) and x0.ndim == 1, "Error: 'x0' argument must be a 1D numpy array."
             assert x0.size >= 2, "Error: 'x0' argument must have at least 2 elements."
             assert callable(pc), "Error: 'pc' argument must be callable."
-            def F(u0, T):
-                tArr = np.linspace(0, T, 1000)
-                sol = solve_ivp(ode, t_span=(0, T), y0=u0, method='RK45', **params)
-                return sol.y[:, -1]
+
             T = x0[-1]
             u0 = x0[:-1]
             assert isinstance(T, float) or isinstance(T, int), "Error: 'T' argument must be a float or an integer."
-            g = np.append(u0 - F(u0, T), pc(u0, **params))
+
+            # Directly calculate the residuals
+            g = np.append(
+                u0 - solve_ivp(ode, t_span=(0, T), y0=u0, method='RK45', **params).y[:, -1],
+                pc(u0, **params)
+            )
+
             assert isinstance(g, np.ndarray) and g.ndim == 1, "Error: 'g' must be a 1D numpy array."
             return g
+
         return G
+
     shooting_output = shooting(ode,u0, pc, fsolve)
     t_span = [0, shooting_output[-1]]
     # Extract the final time and initial guess from the shooting output
@@ -55,16 +61,6 @@ def plot_solutions(ode,pc,u0,args=(), max_step=0.01):
     plt.ylabel('Population')
     plt.legend()
 
-## main
-
-#%% Plots for Q1, might be useful for Jupyter.
-import numpy as np
-from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
-import numdifftools as nd
-from ODE import solve_ode
-#%%
-
 
 
 def pred_prey_plot(a,d,bs,f):
@@ -82,24 +78,56 @@ def pred_prey_plot(a,d,bs,f):
         plt.ylabel('Population')
         plt.show()
 
-#pred_prey_plot(a,d,bs)
+
 
 def shooting_generalised(f):
-    def G(u0_T, pc, *pars):
-        def F(u0, T):
-            # Create a list of time values to solve over
-            t_eval = np.linspace(0, T, 1000)
-            sol =solve_ode(f, u0, t_eval, 0.01, 'RK4', True, *pars) 
-            # Extract the final solution value
-            final_sol = sol[:, -1]
+    """
+    Returns a function G that defines the residual of a boundary value problem, suitable for use with a root-finding
+    algorithm like scipy.optimize.root. The boundary value problem is defined by the function f, which represents a
+    system of ordinary differential equations. The function G takes arguments u0_T, pc, *pars, where u0_T is a numpy
+    array containing the initial state and the final time, pc is a function that computes the residuals at the boundary
+    condition, and *pars are additional parameters that can be passed to the functions f and pc.
+    
+    Args:
+        f: function defining the system of ODEs, takes arguments (t, x, *pars) where t is the current time, x is the
+            current state, and *pars are additional parameters
+            
+    Returns:
+        residual: function defining the residual of the boundary value problem, takes arguments u0_T, pc, *pars, where u0_T
+            is a numpy array containing the initial state and the final time, pc is a function that computes the
+            residuals at the boundary condition, and *pars are additional parameters that can be passed to the functions
+            f and pc
+    """
 
-            # return the final solution value
-            return final_sol
-        # Extract the inputted time and initial values
-        T = u0_T[-1]
-        u0= u0_T[:-1]
-        return np.append(u0 - F(u0, T), pc(u0, *pars))
-    return G
+    def residual(u0_T, pc, *pars):
+        """
+        Computes the residual of a boundary value problem for a system of ODEs defined by the function f. The boundary
+        value problem consists of finding the initial state u0 and final time T that satisfy the boundary conditions
+        specified by the function pc. Additional parameters can be passed to the functions f and pc using *pars.
+        
+        Args:
+            u0_T: numpy array containing the initial state and the final time, in that order
+            pc: function that computes the residuals at the boundary condition, takes arguments (u0, *pars) where u0 is
+                the initial state and *pars are additional parameters
+            *pars: additional parameters to pass to the functions f and pc
+            
+        Returns:
+            numpy array containing the residual of the boundary value problem, given u0_T and pc
+        """
+        
+        # Create a list of time values to solve over
+        t_eval = np.linspace(0, u0_T[-1], 1000)
+        
+        # Solve the ODE using solve_ode, and directly calculate the residuals
+        return np.append(
+            u0_T[:-1] - solve_ode(f, u0_T[:-1], t_eval, 0.01, 'RK4', True, *pars)[:, -1],
+            pc(u0_T[:-1], *pars)
+        )
+    
+    return residual
+
+
+
 
 def plot_hopf_shooting(sol, pc, beta,normal_hopf):
     # Define the time vector.
