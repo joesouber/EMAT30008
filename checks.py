bf58c709a8bdedb0b0703d99a1cf4146d6ca420c
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-26 17:52:43 +0100

    docstrings

--- checks.py
+++ checks.py
@@ -1,14 +1,30 @@
 import numpy  as  np
-#ODE
+
+
 def check_callable(f):
+    
+    '''
+    This function checks if the input function is callable.
+    '''
+    
     if not callable(f):
         raise TypeError("Input function is not callable")
 
 def check_numeric(x0, max_step):
+    
+    '''
+    This function checks if the initial values and maximum step size are numeric.
+    '''
+
     if not isinstance(x0, (int, float)) or not isinstance(max_step, (int, float)):
         raise TypeError("Initial values and maximum step size must be numeric")
 
 def check_pars(pars):
+    
+    '''
+    This function checks if the input parameters are a tuple.
+    '''
+
     if not isinstance(pars, tuple):
         raise TypeError("Input parameters must be a tuple")
 
