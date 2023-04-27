d7eb4d490e593b3a4a81bb24b2497f7319d94b65
joesouber <78925833+joesouber@users.noreply.github.com>
2023-04-27 13:50:20 +0100

    simple ode system animated

--- animate.py
+++ animate.py
@@ -7,8 +7,13 @@ from scipy.sparse.linalg import spsolve
 from scipy.optimize import fsolve
 from PDE import *
 from matplotlib.animation import FuncAnimation
-#from plots import animate_solution
+
 def main():
+    '''
+    This function is used to animate the solutions of the Bratu equation, 
+    the Allen-Cahn equation and the simple diffusion equation.
+    '''
+
     def animate_solution(u, t, L, Title=''):
         fig, ax = plt.subplots()
         x = np.linspace(0, L, u.shape[0])
@@ -47,7 +52,7 @@ def main():
             y = np.exp(mu*u)
             return y
 
-        u,t = finite_difference(L, T, mx, mt, 'dirichlet', dirichlet_0, Initial_Conds, discretisation='beuler', source_term = nonlinear_source, D=0.1, linearity='nonlinear')
+        u,t = finite_difference(L, T, mx, mt, 'dirichlet', dirichlet_0, Initial_Conds, discretisation='implicit', source_term = nonlinear_source, D=0.1, linearity='nonlinear')
 
         animate_solution(u,t,L,'Dynamic Bratu')
 
@@ -76,12 +81,46 @@ def main():
             y = u - u**3
             return y
 
-        u,t = finite_difference(L, T, mx, mt, 'neumann', dirichlet_0, Initial_Conds, discretisation='beuler', source_term = nonlinear_source, D=0.0001, linearity='nonlinear')
+        u,t = finite_difference(L, T, mx, mt, 'neumann', dirichlet_0, Initial_Conds, discretisation='implicit', source_term = nonlinear_source, D=0.0001, linearity='nonlinear')
        
 
         animate_solution(u,t,L,'Allen Cahn')
+    def simple_animate():
 
+        L = 1 # length of the domain
+        T = 0.5 # total time
+        mx = 100 # number of grid points in space
+        mt = 1000 # number of grid points in time
+
+        def u_exact(x, t,D,L):  
+            y = np.exp(-D*(np.pi**2/L**2)*t)*np.sin(np.pi*x/L)
+            return y
+
+        # Dirichlet boundary conditions, 0 on both ends.
+        def dirichlet_0(x, t):
+            return 0
+
+        # Initial condition, sin(pi*x/L).
+        def Initial_Condition(x, L):
+            y = (np.sin(np.pi*x/L))
+            return y
+
+        # Diffusion coefficient,needs to be a function of x, to be compatible with the finite difference method.
+        def D(x):
+            return x / (x * 10)
+
+        # Source term, needs to be a function of x and t, to be compatible with the finite difference method. 0 for this demonstration.
+        def source_term(x,t):
+            return 0
+
+        u, t = finite_difference(L, T, mx, mt, 'dirichlet', dirichlet_0, Initial_Condition, discretisation='explicit',source_term = source_term, D = D, linearity='linear')
+
+        animate_solution(u,t,L,'Simple Diffusion')
+    
     Bratu_animate()
     Allen_cahn_animate()
+    simple_animate()
+
+
 if __name__ == '__main__':
     main()
